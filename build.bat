2bd858e62d7e0c42fa06ef4c195ae97e558103bc
CawaRUS <161507844+CawaRUS@users.noreply.github.com>
2026-03-22 02:33:56 +0300

    fix bugs building

--- build.bat
+++ build.bat
@@ -1,36 +1,38 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 chcp 65001 >nul
 
-set MAKE=.\make\make.exe
+set "MAKE=.\make\make.exe"
 
-for /f "tokens=2 delims=:" %%i in ('chcp') do set CP=%%i
-set CP=%CP: =%
+for /f "tokens=2 delims=:." %%i in ('chcp') do (
+    set "CP=%%i"
+    set "CP=!CP: =!"
+)
 
-set LANGMODE=EN
-if "%CP%"=="866" set LANGMODE=RU
-if "%CP%"=="1251" set LANGMODE=RU
-if "%CP%"=="65001" set LANGMODE=RU
+set "LANGMODE=EN"
+if "%CP%"=="866" set "LANGMODE=RU"
+if "%CP%"=="1251" set "LANGMODE=RU"
+if "%CP%"=="65001" set "LANGMODE=RU"
 
 if "%LANGMODE%"=="RU" (
-    set MSG_THREADS=Обнаружено потоков CPU:
-    set MSG_RECOMMENDED=Рекомендуемое количество:
-    set MSG_ENTER=Введите количество потоков (Enter =
-    set MSG_MENU1=1 - Сборка
-    set MSG_MENU2=2 - Сборка и запуск
-    set MSG_MENU3=3 - Очистка
-    set MSG_CHOICE=Выберите действие:
-    set MSG_DONE=Сборка завершена за
+    set "MSG_THREADS=Обнаружено потоков CPU:"
+    set "MSG_RECOMMENDED=Рекомендуемое количество:"
+    set "MSG_ENTER=Введите количество потоков (Enter ="
+    set "MSG_MENU1=1 - Сборка"
+    set "MSG_MENU2=2 - Сборка и запуск"
+    set "MSG_MENU3=3 - Очистка"
+    set "MSG_CHOICE=Выберите действие: "
+    set "MSG_DONE=Сборка завершена за"
 ) else (
-    set MSG_THREADS=Detected CPU threads:
-    set MSG_RECOMMENDED=Recommended threads:
-    set MSG_ENTER=Enter number of threads (Enter =
-    set MSG_MENU1=1 - Build
-    set MSG_MENU2=2 - Build and run
-    set MSG_MENU3=3 - Clean
-    set MSG_CHOICE=Select action:
-    set MSG_DONE=Build finished in
+    set "MSG_THREADS=Detected CPU threads: "
+    set "MSG_RECOMMENDED=Recommended threads: "
+    set "MSG_ENTER=Enter number of threads (Enter ="
+    set "MSG_MENU1=1 - Build"
+    set "MSG_MENU2=2 - Build and run"
+    set "MSG_MENU3=3 - Clean"
+    set "MSG_CHOICE=Select action: "
+    set "MSG_DONE=Build finished in"
 )
 
 echo.
@@ -42,8 +44,8 @@ if %DEFAULT_THREADS% LSS 1 set DEFAULT_THREADS=1
 echo %MSG_RECOMMENDED% %DEFAULT_THREADS%
 echo.
 
-set /p THREADS=%MSG_ENTER% %DEFAULT_THREADS%): 
-if "%THREADS%"=="" set THREADS=%DEFAULT_THREADS%
+set /p "THREADS=%MSG_ENTER% %DEFAULT_THREADS%): "
+if "%THREADS%"=="" set "THREADS=%DEFAULT_THREADS%"
 
 echo.
 echo %MSG_MENU1%
@@ -51,17 +53,17 @@ echo %MSG_MENU2%
 echo %MSG_MENU3%
 echo.
 
-set /p ACTION=%MSG_CHOICE%
+set /p "ACTION=%MSG_CHOICE%"
 
 echo.
 
-set START=%TIME%
+set "START=%TIME%"
 
-if "%ACTION%"=="1" %MAKE% -j%THREADS%
-if "%ACTION%"=="2" %MAKE% -j%THREADS% run
-if "%ACTION%"=="3" %MAKE% clean
+if "%ACTION%"=="1" "%MAKE%" -j%THREADS%
+if "%ACTION%"=="2" "%MAKE%" -j%THREADS% run
+if "%ACTION%"=="3" "%MAKE%" clean
 
-set END=%TIME%
+set "END=%TIME%"
 
 echo.
 echo %MSG_DONE% %START% ^> %END%
